6ff73169698bcc7b84459ebb26d9a37f7eae445b
Nathan Heidt <nathanielheidt@gmail.com>
2016-01-27 13:11:57 -0800

    added some custom js files, started integrating with angularjs

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var app = angular.module('portfolioapp', ['ui.bootstrap']);
+
+  app.controller('portfolio_projects', function($scope){
+  $scope.myInterval = 10000;
+  $scope.slides = [
+    {
+      image: 'http://lorempixel.com/400/200/'
+    },
+    {
+      image: 'http://lorempixel.com/400/200/food'
+    },
+    {
+      image: 'http://lorempixel.com/400/200/sports'
+    },
+    {
+      image: 'http://lorempixel.com/400/200/people'
+    }
+  ];
+});
