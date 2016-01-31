7cd26ce2a6ecfe6aeb7b0e31bedef82cdc3bb1db
Nathan Heidt <nathanielheidt@gmail.com>
2016-01-30 23:43:56 -0800

    added a meta doc file for holding project data

--- js/app.js
+++ js/app.js
@@ -1,19 +1,11 @@
 var app = angular.module('portfolioapp', ['ui.bootstrap']);
 
-  app.controller('portfolio_projects', function($scope){
+  app.controller('portfolio_projects', function($scope, $http){
+
+  $http.get('../project_meta/projectmeta.json').success(function(data) {
+     $scope.projects = data.projects;
+     console.log($scope.projects);
+  });
+
   $scope.myInterval = 10000;
-  $scope.slides = [
-    {
-      image: 'http://lorempixel.com/400/200/'
-    },
-    {
-      image: 'http://lorempixel.com/400/200/food'
-    },
-    {
-      image: 'http://lorempixel.com/400/200/sports'
-    },
-    {
-      image: 'http://lorempixel.com/400/200/people'
-    }
-  ];
 });
